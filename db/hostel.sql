00fdc69589aaa3915c52c04bf043a3c5a3d66737
Foysal Ahammad <32242259+foysal05@users.noreply.github.com>
2019-08-31 23:46:53 +0600

    sql updated

--- db/hostel.sql
+++ db/hostel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2019 at 08:07 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.7
+-- Generation Time: Aug 31, 2019 at 07:41 PM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,6 +36,15 @@ CREATE TABLE `complain` (
   `status` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
+--
+-- Dumping data for table `complain`
+--
+
+INSERT INTO `complain` (`c_id`, `complain`, `std_id`, `date`, `status`) VALUES
+(2, '<p><strong>New Complain</strong></p>\r\n', 1000171, '2019-07-24 23:44:45', 'Done'),
+(3, '<p>fgfdgdgdfg</p>\r\n', 1000171, '2019-07-25 10:53:58', 'Pending'),
+(4, 'new complain', 1000171, '2019-07-25 10:57:59', 'Pending');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +62,15 @@ CREATE TABLE `guardian` (
   `g_password` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
+--
+-- Dumping data for table `guardian`
+--
+
+INSERT INTO `guardian` (`sid_id`, `g_name`, `relation`, `g_address`, `g_email`, `g_phone`, `g_nid`, `g_password`) VALUES
+(1, 'Payer Ahamed', 'Father', 'comilla', 'foysalahammed055@gmail.com', '5103247049', 2147483647, 'Tkt2CTz3'),
+(2, 'DW Assignment', 'Self', 'Panthapat', 'foysal.int@gmail.com', '1852595966', 4567, 'sZYS9KBk'),
+(3, 'gfhgfh', 'fghfgh', 'fghfghfgh', 'foysal.int@gmail.com', '5103247049', 2147483647, 'TxndVagN');
+
 -- --------------------------------------------------------
 
 --
@@ -66,6 +84,18 @@ CREATE TABLE `leave_application` (
   `application` varchar(2000) DEFAULT NULL,
   `status` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `leave_application`
+--
+
+INSERT INTO `leave_application` (`l_id`, `applicant_id`, `date`, `application`, `status`) VALUES
+(1, 1000171, '2019-07-23 11:35:30', '<p>new application</p>\r\n', 'Pending'),
+(2, 1000171, '2019-07-23 11:38:25', '<p>tyuygjgjgh</p>\r\n', 'Pending'),
+(3, 1000171, '2019-07-23 11:39:11', '<p>ghjjghjghvdfvgd</p>\r\n', 'Pending'),
+(4, 1000171, '2019-07-23 11:41:23', '<p>jkkhjkjhkhjk</p>\r\n', 'Pending'),
+(5, 1000171, '2019-07-23 11:45:12', '<p>new configaration</p>\r\n', 'Pending'),
+(6, 1000171, '2019-07-23 11:58:26', '<p>Subject: Annual leave application</p>\r\n\r\n<p>Dear Mr./Mrs. {Recipient&rsquo;s Name},</p>\r\n\r\n<p>I am writing this letter to let you know that I am in need of a long-term leave. Thus, I would like to avail my full annual leave allotment as I have my complete yearly leave allowance.</p>\r\n\r\n<p>I request you to consider my leave application of thirty days as I am planning for an international vacation with my family. I would like to avail the leaves from {start date} to {end date}.</p>\r\n\r\n<p>I have delegated my current project to {person&#39;s name}. He/she very well understands about my project and is capable of handling the task without any difficulties. In fact, it is only the final part that is left to be carried out by him/her.</p>\r\n\r\n<p>During the days of my absence from office, I can be reached at {email address/contact number}.</p>\r\n\r\n<p>I will return to the office on {date}. In case I want to resume the work sooner or later than the stated date, I will let you know well in advance.</p>\r\n\r\n<p>Yours Sincerely,<br />\r\n{Your Name}</p>\r\n', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -101,8 +131,41 @@ CREATE TABLE `payment` (
   `due` int(5) DEFAULT NULL,
   `month` varchar(20) DEFAULT NULL,
   `year` varchar(5) DEFAULT NULL,
-  `received_by` varchar(100) DEFAULT NULL
+  `received_by` varchar(100) DEFAULT NULL,
+  `trxid` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`p_id`, `s_id`, `date`, `payable_bill`, `amount`, `due`, `month`, `year`, `received_by`, `trxid`) VALUES
+(1, 1000171, '07/23/2019', 3000, 3000.00, 0, 'January', '2019', 'Office', NULL),
+(2, 1000171, '07/23/2019', 3000, 3000.00, 0, 'February', '2019', 'Office', NULL),
+(3, 1000171, '07/25/2019', 4000, 4000.00, 0, 'March', '2019', 'Office', '4757567');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_request`
+--
+
+CREATE TABLE `payment_request` (
+  `op_id` int(11) NOT NULL,
+  `payment_id` int(11) DEFAULT NULL,
+  `amount` int(10) DEFAULT NULL,
+  `std_id` int(11) DEFAULT NULL,
+  `trxid` int(50) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment_request`
+--
+
+INSERT INTO `payment_request` (`op_id`, `payment_id`, `amount`, `std_id`, `trxid`, `date`, `status`) VALUES
+(1, 2, 1000, 1000171, 67567567, '2019-07-25 21:04:26', 'Done');
 
 -- --------------------------------------------------------
 
@@ -123,11 +186,10 @@ CREATE TABLE `room` (
 --
 
 INSERT INTO `room` (`r_id`, `room_no`, `room_capacity`, `room_video`, `seat_price`) VALUES
-(1, 201, 2, 'videos/201.mp4', '5000.00'),
-(3, 202, 4, 'videos/202.mp4', '5500.00'),
-(4, 203, 3, 'videos/203.mp4', '5000.00'),
-(5, 204, 1, 'videos/204.mp4', '2500.00'),
-(7, 205, 3, 'videos/205.mp4', '3000.00');
+(5, 204, 1, 'videos/204.mp4', '5000.00'),
+(7, 205, 3, 'videos/205.mp4', '4000.00'),
+(8, 201, 4, 'videos/201.mp4', '4000.00'),
+(9, 202, 3, 'videos/202.mp4', '5000.00');
 
 -- --------------------------------------------------------
 
@@ -154,6 +216,27 @@ INSERT INTO `staff` (`id`, `fullname`, `email`, `password`, `type`, `status`) VA
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `std_category`
+--
+
+CREATE TABLE `std_category` (
+  `pc_id` int(11) NOT NULL,
+  `pc_name` varchar(5) DEFAULT NULL,
+  `amount` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `std_category`
+--
+
+INSERT INTO `std_category` (`pc_id`, `pc_name`, `amount`) VALUES
+(1, 'A', 2500),
+(2, 'B', 3750),
+(3, 'C', 5000);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `std_seat`
 --
 
@@ -162,6 +245,14 @@ CREATE TABLE `std_seat` (
   `std_id` int(11) DEFAULT NULL,
   `room` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `std_seat`
+--
+
+INSERT INTO `std_seat` (`sb_id`, `std_id`, `room`) VALUES
+(2, 1000175, 204),
+(6, 1000171, 205);
 
 -- --------------------------------------------------------
 
@@ -185,6 +276,14 @@ CREATE TABLE `student` (
   `category` varchar(5) DEFAULT NULL,
   `date` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`std_id`, `v_id`, `fullname`, `photo`, `father_name`, `mother_name`, `nid`, `email`, `phone`, `permanent_address`, `additional_info`, `password`, `category`, `date`) VALUES
+(1, '1000171', 'Foysal Ahammad', 'photos/1000171.jpg', 'Payer Ahamed', 'Syeda Selina Begum', 2147483647, '1000171@daffodil.ac', '01852595966', 'Comilla', 'n/a', '123456', '', '07/23/2019'),
+(2, '1000175', 'DW Assignment', 'photos/1000175.png', 'Mr. Father', 'Mrs. Mother', 2147483647, 'foysal.int@gmail.com', '1852595966', 'Panthapat', '', 'CpH9IQAW', '', '07/23/2019');
 
 --
 -- Indexes for dumped tables
@@ -221,6 +320,12 @@ ALTER TABLE `payment`
   ADD PRIMARY KEY (`p_id`) USING BTREE;
 
 --
+-- Indexes for table `payment_request`
+--
+ALTER TABLE `payment_request`
+  ADD PRIMARY KEY (`op_id`);
+
+--
 -- Indexes for table `room`
 --
 ALTER TABLE `room`
@@ -231,6 +336,12 @@ ALTER TABLE `room`
 --
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `std_category`
+--
+ALTER TABLE `std_category`
+  ADD PRIMARY KEY (`pc_id`) USING BTREE;
 
 --
 -- Indexes for table `std_seat`
@@ -252,7 +363,7 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `complain`
 --
 ALTER TABLE `complain`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `leave_application`
@@ -270,13 +381,19 @@ ALTER TABLE `notice`
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `payment_request`
+--
+ALTER TABLE `payment_request`
+  MODIFY `op_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `staff`
@@ -285,10 +402,16 @@ ALTER TABLE `staff`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `std_category`
+--
+ALTER TABLE `std_category`
+  MODIFY `pc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `std_seat`
 --
 ALTER TABLE `std_seat`
-  MODIFY `sb_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `sb_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `student`
